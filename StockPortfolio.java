34c4b7d6086335e16e926ae1a9c2797501ae9a53
Abhinav Vishwakarma <91567910+abhi9Vishwa@users.noreply.github.com>
2024-11-30 09:52:04 +0000

    group 5

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -4,26 +4,40 @@ class StockPortfolio {
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
     // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
         // TODO
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.highestPrice = buyingPrice;
+        this.currentPrice = buyingPrice;
+        this.buyingPrice = buyingPrice;
     }
 
     // TODO: Update current market price
     public void updatePrice(double newPrice) {
         // TODO
+        if(newPrice >= 0.0){
+            this.currentPrice  = newPrice;
+            if(newPrice > this.highestPrice) this.highestPrice = newPrice;
+        }
     }
 
     // TODO: Calculate total profit or loss
     public double calculateProfit() {
         // TODO
-        return 0.0;
+        double profit = 0;
+        double diff = (currentPrice - buyingPrice);
+        profit = diff*quantity;
+        return profit;
     }
 
     // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
         // TODO
-        return 0.0;
+        double totValue = currentPrice*quantity;
+        return totValue;
     }
 }
