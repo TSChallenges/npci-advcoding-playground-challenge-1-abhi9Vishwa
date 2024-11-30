cada072a07cc9cb889122cd409f7a5d982c194a8
Abhinav Vishwakarma <91567910+abhi9Vishwa@users.noreply.github.com>
2024-11-30 09:53:17 +0000

    group 5 change 1

--- Main.java
+++ Main.java
@@ -2,16 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Test Case 1: Stock Analysis
         StockPortfolio stock = new StockPortfolio("AAPL", 10, 130.0);
-        stock.updatePrice(-30.0);
-        System.out.println(stock.calculateProfit());
-        System.out.println(stock.getCurrentValue());
-        stock.updatePrice(30.0);
-        System.out.println(stock.calculateProfit());
-        System.out.println(stock.getCurrentValue());
-        stock.updatePrice(0.0);
-        System.out.println(stock.calculateProfit());
-        System.out.println(stock.getCurrentValue());
-        stock.updatePrice(300.0);
+        stock.updatePrice(150.0);
         System.out.println(stock.calculateProfit());
         System.out.println(stock.getCurrentValue());
     }
